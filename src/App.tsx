397255373549392f1ed7c05330256461043abd59
Min3th <minethweerasinghe@gmail.com>
2025-07-10 18:09:02 +0530

    add minor prettier configs

--- src/App.tsx
+++ src/App.tsx
@@ -42,18 +42,15 @@ function App() {
   };
 
   return (
-    <div className="h-full w-full scroll-smooth bg-white dark:bg-black transition-colors duration-300 overflow-x-hidden">
+    <div className="h-full w-full overflow-x-hidden scroll-smooth bg-white transition-colors duration-300 dark:bg-black">
       <ToastContainer autoClose={4000} hideProgressBar theme="dark" />
       <Navbar />
-      <div className="w-screen h-screen flex flex-col md:flex-row relative ">
-        <div className="w-full md:w-[550px] h-1/3 md:h-screen flex flex-row relative bg- ">
-          <img
-            src={profile}
-            className="ml-20 md:ml-[200px] transform scale-[1.2] absolute top-[50px] md:top-[200px]"
-          />
+      <div className="relative flex h-screen w-screen flex-col md:flex-row">
+        <div className="bg- relative flex h-1/3 w-full flex-row md:h-screen md:w-[550px]">
+          <img src={profile} className="absolute top-[50px] ml-20 scale-[1.2] transform md:top-[200px] md:ml-[200px]" />
         </div>
-        <div className="flex-1 h-2/3 md:h-screen flex flex-col justify-center items-center px-5 ">
-          <div className="text-[28px] sm:text-[40px] md:text-[50px] font-[sans-serif] text-black dark:text-white text-center">
+        <div className="flex h-2/3 flex-1 flex-col items-center justify-center px-5 md:h-screen">
+          <div className="text-center font-[sans-serif] text-[28px] text-black dark:text-white sm:text-[40px] md:text-[50px]">
             <ReactTyped
               strings={["I'm Mineth Weerasinghe"]}
               typeSpeed={50}
@@ -63,34 +60,34 @@ function App() {
               loop
             />
           </div>
-          <div className="text-[16px] sm:text-[18px] text-center font-[sans-serif] tracking-[0.06rem] dark:text-white mt-2">
+          <div className="mt-2 text-center font-[sans-serif] text-[16px] tracking-[0.06rem] dark:text-white sm:text-[18px]">
             Software Engineer | Full Stack Developer
           </div>
-          <div className="text-[14px] sm:text-[16px] md:text-[18px] text-center mt-2 dark:text-white px-4 md:px-0">
-            Third year Computer Science & Engineering student at the University
-            of Moratuwa, interested in Software Engineering & web development
+          <div className="mt-2 px-4 text-center text-[14px] dark:text-white sm:text-[16px] md:px-0 md:text-[18px]">
+            Third year Computer Science & Engineering student at the University of Moratuwa, interested in Software
+            Engineering & web development
           </div>
 
-          <div className="flex flex-row items-center justify-center gap-4 mt-5 flex-wrap">
+          <div className="mt-5 flex flex-row flex-wrap items-center justify-center gap-4">
             <i
-              className="fa-solid fa-phone text-[30px] md:text-[35px] z-10 cursor-pointer dark:text-white"
+              className="fa-solid fa-phone z-10 cursor-pointer text-[30px] dark:text-white md:text-[35px]"
               onClick={() => copyToClip("0762545500")}
             ></i>
 
             <i
-              className="fa-solid fa-envelope z-10 cursor-pointer text-[35px] md:text-[40px] dark:text-white"
+              className="fa-solid fa-envelope z-10 cursor-pointer text-[35px] dark:text-white md:text-[40px]"
               onClick={() => copyToClip("mineth.21@cse.mrt.ac.lk")}
             ></i>
 
             <a
               href="https://www.linkedin.com/in/mineth-weerasinghe-2551b0241"
               target="_blank"
-              className="fa-brands fa-linkedin text-[35px] md:text-[40px] dark:text-white"
+              className="fa-brands fa-linkedin text-[35px] dark:text-white md:text-[40px]"
             ></a>
             <a
               href="https://github.com/Min3th"
               target="_blank"
-              className="fa-brands fa-github text-[35px] md:text-[40px] dark:text-white"
+              className="fa-brands fa-github text-[35px] dark:text-white md:text-[40px]"
             ></a>
 
             <div className="h-10 w-[1px] bg-black dark:bg-white"></div>
@@ -98,7 +95,7 @@ function App() {
             <a
               href="/My_CV.pdf"
               download="My_CV.pdf"
-              className="rounded mt-2 p-2 sm:p-3 border-[1.3px] border-black bg-[#FCFCFF] font-medium hover:border-[0px] hover:scale-[1.1] transition duration-[250] ease-in-out hover:bg-white hover:shadow dark:bg-black dark:border-white dark:text-white"
+              className="mt-2 rounded border-[1.3px] border-black bg-[#FCFCFF] p-2 font-medium transition duration-[250] ease-in-out hover:scale-[1.1] hover:border-[0px] hover:bg-white hover:shadow dark:border-white dark:bg-black dark:text-white sm:p-3"
             >
               Download CV
             </a>
@@ -106,38 +103,27 @@ function App() {
         </div>
       </div>
 
-      <div
-        id="about"
-        className="flex flex-col relative justify-center items-center "
-      >
-        <div className="rounded-[40px] h-screen  flex-col flex items-center p-20  justify-center sm:w-screen w-[400px] sm:mt-0 mt-[200px] text-center">
-          <div className="font-[sans-serif] font-semibold text-[60px] dark:text-white">
-            About Me
-          </div>
-          <div className="text-center text-[20px] dark:text-white m-10  ">
-            I am passionate about becoming a software engineer and provide value
-            to the web development space.I am interested in full stack
-            applications , and providing solutions to problems.I am team
-            player,being a part of the University badminton team that won many
-            tournaments.I am also a member of several clubs, and has lead many
-            projects to success. And I wouldn't say no to a choclate milkshake
-            🧋.
+      <div id="about" className="relative flex flex-col items-center justify-center">
+        <div className="mt-[200px] flex h-screen w-[400px] flex-col items-center justify-center rounded-[40px] p-20 text-center sm:mt-0 sm:w-screen">
+          <div className="font-[sans-serif] text-[60px] font-semibold dark:text-white">About Me</div>
+          <div className="m-10 text-center text-[20px] dark:text-white">
+            I am passionate about becoming a software engineer and provide value to the web development space.I am
+            interested in full stack applications , and providing solutions to problems.I am team player,being a part of
+            the University badminton team that won many tournaments.I am also a member of several clubs, and has lead
+            many projects to success. And I wouldn't say no to a choclate milkshake 🧋.
           </div>
         </div>
       </div>
       <div
         id="projects"
-        className="h-full  flex flex-col relative justify-center items-center sm:w-screen w-[400px] sm:mt-0 mt-[200px] text-center"
+        className="relative mt-[200px] flex h-full w-[400px] flex-col items-center justify-center text-center sm:mt-0 sm:w-screen"
       >
-        <div className="font-semibold font-[sans-serif] text-[50px] mt-12 dark:text-white">
-          My Projects
-        </div>
+        <div className="mt-12 font-[sans-serif] text-[50px] font-semibold dark:text-white">My Projects</div>
         <div className="text-[20px] dark:text-white">
-          Below are some of the projects I have worked on as a Computer Science
-          undergraduate
+          Below are some of the projects I have worked on as a Computer Science undergraduate
         </div>
         <div className="flex flex-col items-center justify-center">
-          <div className="flex flex-col md:flex-row items-center justify-center sm:gap-4 gap-10 m-5">
+          <div className="m-5 flex flex-col items-center justify-center gap-10 sm:gap-4 md:flex-row">
             <Card
               link="https://github.com/CS3203-SEP-21-Group-22"
               imageUrl={ims}
@@ -153,7 +139,7 @@ function App() {
               symbols={[react, node]}
             />
           </div>
-          <div className="flex flex-col md:flex-row items-center justify-center sm:gap-4 gap-10 m-5">
+          <div className="m-5 flex flex-col items-center justify-center gap-10 sm:gap-4 md:flex-row">
             <Card
               link="https://min3th.github.io/TasksApp/"
               imageUrl={alarm}
@@ -173,70 +159,67 @@ function App() {
       </div>
       <div
         id="tech"
-        className=" h-screen  flex flex-col relative p-20 justify-center items-center sm:w-screen w-[400px] sm:mt-0 mt-[900px] text-center"
+        className="relative mt-[900px] flex h-screen w-[400px] flex-col items-center justify-center p-20 text-center sm:mt-0 sm:w-screen"
       >
-        <div className="  flex-col flex justify-center items-center gap-2 dark:text-white ">
-          <p className="font-[sans-serif] font-semibold text-[50px] ">
-            Technologies
-          </p>
+        <div className="flex flex-col items-center justify-center gap-2 dark:text-white">
+          <p className="font-[sans-serif] text-[50px] font-semibold">Technologies</p>
 
-          <p className="text-[20px] text-center">
-            Below are some of the technologies I am familiar with and have used
-            in my work.
+          <p className="text-center text-[20px]">
+            Below are some of the technologies I am familiar with and have used in my work.
           </p>
         </div>
-        <div className=" flex flex-col dark:shadow-lg dark:shadow-gray-800 shadow-lg justify-center items-center">
-          <div className="flex flex-wrap  justify-center items-center gap-4 dark:text-white">
-            <div className="flex flex-col items-center relative w-[120px] h-[120px] md:w-[140px] md:h-[140px] hover:scale-[1.1] duration-200 ease-in-out">
-              <img src={net} className="transform scale-[0.5]" />
+        <div className="flex flex-col items-center justify-center shadow-lg dark:shadow-lg dark:shadow-gray-800">
+          <div className="flex flex-wrap items-center justify-center gap-4 dark:text-white">
+            <div className="relative flex h-[120px] w-[120px] flex-col items-center duration-200 ease-in-out hover:scale-[1.1] md:h-[140px] md:w-[140px]">
+              <img src={net} className="scale-[0.5] transform" title="ASP.NET" />
             </div>
-            <div className="flex flex-col items-center w-[120px] h-[120px] md:w-[140px] md:h-[140px] hover:scale-[1.1] duration-200 ease-in-out">
-              <img src={vite} className="transform scale-[0.5]" />
+            <div className="flex h-[120px] w-[120px] flex-col items-center duration-200 ease-in-out hover:scale-[1.1] md:h-[140px] md:w-[140px]">
+              <img src={vite} className="scale-[0.5] transform" title="Vite" />
             </div>
-            <div className="flex flex-col items-center w-[120px] h-[120px] md:w-[140px] md:h-[140px] hover:scale-[1.1] duration-200 ease-in-out">
-              <img src={azure} className="transform scale-[0.6] mt-5" />
+            <div className="flex h-[120px] w-[120px] flex-col items-center duration-200 ease-in-out hover:scale-[1.1] md:h-[140px] md:w-[140px]">
+              <img src={azure} className="mt-5 scale-[0.6] transform" title="Azure" />
             </div>
-            <div className="flex flex-col items-center w-[120px] h-[120px] md:w-[140px] md:h-[140px] hover:scale-[1.1] duration-200 ease-in-out">
-              <img src={csharp} className="transform scale-[0.9] mt-1" />
+            <div className="flex h-[120px] w-[120px] flex-col items-center duration-200 ease-in-out hover:scale-[1.1] md:h-[140px] md:w-[140px]">
+              <img src={csharp} className="mt-1 scale-[0.9] transform" title="C#" />
             </div>
-            <div className="flex flex-col items-center w-[120px] h-[120px] md:w-[140px] md:h-[140px] hover:scale-[1.1] duration-200 ease-in-out">
-              <img src={cpp} className="transform scale-[0.55] mt-2" />
+            <div className="flex h-[120px] w-[120px] flex-col items-center duration-200 ease-in-out hover:scale-[1.1] md:h-[140px] md:w-[140px]">
+              <img src={cpp} className="mt-2 scale-[0.55] transform" title="" />
             </div>
-            <div className="flex flex-col items-center w-[120px] h-[120px] md:w-[140px] md:h-[140px] hover:scale-[1.1] duration-200 ease-in-out">
-              <img src={c} className="transform scale-[0.55] mt-[10px]" />
+            <div className="flex h-[120px] w-[120px] flex-col items-center duration-200 ease-in-out hover:scale-[1.1] md:h-[140px] md:w-[140px]">
+              <img src={c} className="mt-[10px] scale-[0.55] transform" title="C" />
             </div>
-            <div className="flex flex-col items-center w-[120px] h-[120px] md:w-[140px] md:h-[140px] hover:scale-[1.1] duration-200 ease-in-out">
-              <img src={react} className="transform scale-[0.55]" />
+            <div className="flex h-[120px] w-[120px] flex-col items-center duration-200 ease-in-out hover:scale-[1.1] md:h-[140px] md:w-[140px]">
+              <img src={react} className="scale-[0.55] transform" title="ReactJS" />
             </div>
-            <div className="flex flex-col items-center w-[120px] h-[120px] md:w-[140px] md:h-[140px] hover:scale-[1.1] duration-200 ease-in-out">
-              <img src={python} className="transform scale-[0.55]" />
+            <div className="flex h-[120px] w-[120px] flex-col items-center duration-200 ease-in-out hover:scale-[1.1] md:h-[140px] md:w-[140px]">
+              <img src={python} className="scale-[0.55] transform" title="Python" />
             </div>
           </div>
 
-          <div className="flex flex-wrap justify-center items-center gap-4 dark:text-white mt-4 mb-10">
-            <div className="flex flex-col items-center relative w-[120px] h-[120px] md:w-[140px] md:h-[140px] hover:scale-[1.1] duration-200 ease-in-out">
-              <img src={java} className="transform scale-[0.5]" />
+          <div className="mb-10 mt-4 flex flex-wrap items-center justify-center gap-4 dark:text-white">
+            <div className="relative flex h-[120px] w-[120px] flex-col items-center duration-200 ease-in-out hover:scale-[1.1] md:h-[140px] md:w-[140px]">
+              <img src={java} className="scale-[0.5] transform" title="Java" />
             </div>
-            <div className="flex flex-col items-center w-[120px] h-[120px] md:w-[140px] md:h-[140px] hover:scale-[1.1] duration-200 ease-in-out">
-              <img src={html5} className="transform scale-[0.5]" />
+            <div className="flex h-[120px] w-[120px] flex-col items-center duration-200 ease-in-out hover:scale-[1.1] md:h-[140px] md:w-[140px]">
+              <img src={html5} className="scale-[0.5] transform" title="HTML5" />
             </div>
-            <div className="flex flex-col items-center w-[120px] h-[120px] md:w-[140px] md:h-[140px] hover:scale-[1.1] duration-200 ease-in-out">
-              <img src={css3} className="transform scale-[0.5]" />
+            <div className="flex h-[120px] w-[120px] flex-col items-center duration-200 ease-in-out hover:scale-[1.1] md:h-[140px] md:w-[140px]">
+              <img src={css3} className="scale-[0.5] transform" title="CSS3" />
             </div>
-            <div className="flex flex-col items-center w-[120px] h-[120px] md:w-[140px] md:h-[140px] hover:scale-[1.1] duration-200 ease-in-out">
-              <img src={flask} className="transform scale-[0.9]" />
+            <div className="flex h-[120px] w-[120px] flex-col items-center duration-200 ease-in-out hover:scale-[1.1] md:h-[140px] md:w-[140px]">
+              <img src={flask} className="scale-[0.9] transform" title="Flask" />
             </div>
-            <div className="flex flex-col items-center w-[120px] h-[120px] md:w-[140px] md:h-[140px] hover:scale-[1.1] duration-200 ease-in-out">
-              <img src={js} className="transform scale-[0.55]" />
+            <div className="flex h-[120px] w-[120px] flex-col items-center duration-200 ease-in-out hover:scale-[1.1] md:h-[140px] md:w-[140px]">
+              <img src={js} className="scale-[0.55] transform" title="Javascript" />
             </div>
-            <div className="flex flex-col items-center w-[120px] h-[120px] md:w-[140px] md:h-[140px] hover:scale-[1.1] duration-200 ease-in-out">
-              <img src={mysql} className="transform scale-[0.55]" />
+            <div className="flex h-[120px] w-[120px] flex-col items-center duration-200 ease-in-out hover:scale-[1.1] md:h-[140px] md:w-[140px]">
+              <img src={mysql} className="scale-[0.55] transform" title="MySQL" />
             </div>
-            <div className="flex flex-col items-center w-[120px] h-[120px] md:w-[140px] md:h-[140px] hover:scale-[1.1] duration-200 ease-in-out">
-              <img src={ts} className="transform scale-[0.55] mt-8" />
+            <div className="flex h-[120px] w-[120px] flex-col items-center duration-200 ease-in-out hover:scale-[1.1] md:h-[140px] md:w-[140px]">
+              <img src={ts} className="mt-8 scale-[0.55] transform" title="Typescript" />
             </div>
-            <div className="flex flex-col items-center w-[120px] h-[120px] md:w-[140px] md:h-[140px] hover:scale-[1.1] duration-200 ease-in-out">
-              <img src={tailwind} className="transform scale-[0.55] mt-8" />
+            <div className="flex h-[120px] w-[120px] flex-col items-center duration-200 ease-in-out hover:scale-[1.1] md:h-[140px] md:w-[140px]">
+              <img src={tailwind} className="mt-8 scale-[0.55] transform" title="Tailwind" />
             </div>
           </div>
         </div>
